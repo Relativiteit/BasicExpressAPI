98ebfb4e47f565a0cde883bf24e58e0eaf2acc2c
Alejo <alejocain@gmail.com>
2023-01-31 21:48:35 +0100

    added routes and template engine-handlebars

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 const app = express();
 const logger = require('./middleware/logger.js');
+const members = require('./Members');
 
 
 // init middileware 
 // app.use(logger);
 
+
+// Handlebars Middleware 
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
 // Body Parser Middleware 
 app.use(express.json());
 // handle url encoded data 
-app.use(express.urlencoded({extended: false }));
+app.use(express.urlencoded({ extended: false }));
 
-// set static folder
+// Homepage route 
+app.get('/', (req, res) => res.render('index', {
+    title: 'Member App',
+    members
+}));
+
+// Set static folder you can move this above homepage route to rende this instead
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Members API Routes 
 app.use('/api/members', require('./routes/api/members.js'));
 
 const PORT = process.env.PORT || 4000;
--- routes/api/members.js
+++ routes/api/members.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const uuid = require('uuid');
 const router = express.Router();
-const members = require('../../Members'); 
+const members = require('../../Members');
 
 // Gets All Members 
 router.get('/', (req, res) => {
@@ -11,8 +12,8 @@ router.get('/', (req, res) => {
 // GET single member 
 router.get('/:id', (req, res) => {
     const found = members.some(member => member.id === parseInt(req.params.id));
-    
-    if(found) {
+
+    if (found) {
         res.json(members.filter(member => member.id === parseInt(req.params.id)));
     } else {
         res.status(400).json({ msg: `No member with the id of ${req.params.id}` })
@@ -21,11 +22,55 @@ router.get('/:id', (req, res) => {
 
 // Create Member 
 router.post('/', (req, res) => {
-    res.send(req.body);
+    const newMember = {
+        id: uuid.v4(),
+        name: req.body.name,
+        email: req.body.email,
+        status: 'active'
+    };
+
+    if (!newMember.name || !newMember.email) {
+        return res.status(400).json({ message: 'Please incluce a name and email' });
+    }
+
+    members.push(newMember);
+    res.json(members);
+    // this is how you can redirect to page directly. 
+    // res.redirect('/');
 });
 
-router.post('/new', (req, res) => {
-    res.send(req.);
-})
+// Update Member
+
+router.put('/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+
+    if (found) {
+        const updMember = req.body;
+        members.forEach(member => {
+            if (member.id === parseInt(req.params.id)) {
+                // tenary operator condition ? exexuteIfTrue : executeIfFalse
+                member.name = updMember.name ? updMember.name : member.name;
+                member.email = updMember.email ? updMember.email : member.email;
+
+                res.json({ message: 'Member updated', member })
+
+            }
+        });
+
+    } else {
+        res.status(400).json({ msg: `No member with the id of ${req.params.id}` })
+    }
+});
+
+// Delete Member
+router.delete('/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+
+    if (found) {
+        res.json({ msg: 'Member deleted ', members: members.filter(member => member.id !== parseInt(req.params.id)) });
+    } else {
+        res.status(400).json({ msg: `No member with the id of ${req.params.id}` })
+    }
+});
 
 module.exports = router;
